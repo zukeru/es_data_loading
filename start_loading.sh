a873b2c878aae73341ea9bcb6a3c4a0ee6072be1
Grant Zukel <zukeru@gmail.com>
2015-07-07 15:05:55 -0600

    fixed printing issue

--- start_loading.sh
+++ start_loading.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -x
 echo $wos
-pip install elasticsearch
 python $WORKSPACE/es_single_load.py  --secret_key $AWS_SECRET_KEY --access_key $AWS_ACCESS_KEY --threads $threads --shards $shards --replicas $replicas --index $index --host $host --wos $wos
 
