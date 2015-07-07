b3138b23a9fe1f0b33da0c460d53f4d4e3503b24
Grant Zukel <zukeru@gmail.com>
2015-07-07 11:29:17 -0600

    fixed printing issue

--- start_loading.sh
+++ start_loading.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 echo $wos
-python $WORKSPACE/es_load_single.py  --secret_key $AWS_SECRET_KEY --access_key $AWS_ACCESS_KEY --threads $threads --shards $shards --replicas $replicas --index $index --host $host --wos $wos
+python $WORKSPACE/es_single_load.py  --secret_key $AWS_SECRET_KEY --access_key $AWS_ACCESS_KEY --threads $threads --shards $shards --replicas $replicas --index $index --host $host --wos $wos
 
