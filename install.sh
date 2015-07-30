9a0bd9635e4af461692546a0750724fb08be9355
EC2 Default User <ec2-user@ip-172-31-28-189.us-west-2.compute.internal>
2015-07-30 03:31:18 +0000

    updates fixed install script

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 sudo mkdir /opt/vdl
-sudo cp ./init.d/VisionDataLoader /etc/init.d/vdl
+sudo cp ./init.d/vdl /etc/init.d/vdl
 sudo cp ./api_execute_data_load.py /opt/vdl/vdl.py
-print "Starting VDL Server."
+echo "Starting VDL Server."
+{
 sudo update-rc.d vdl defaults
+} ||{
+sudo chkconfig --add vdl
+}
 sudo service vdl start
 
 exit 0
