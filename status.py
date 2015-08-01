cc4f382ef1b978607e83bfa210b44dce72d419e0
zukeru <zukeru@gmail.com>
2015-07-31 20:43:18 -0600

    updates

--- status.py
+++ status.py
@@ -61,6 +61,21 @@ def no_comands():
                 use = to seperate key value pairs
                 use | to insert \
                 """
+                
+@route('/get_status/')
+def no_comands():
+    return """Please include all nessecary values: example: 
+                Start Load
+                http://127.0.0.1:8001/load_data/load&host=ip or DNS&thread=5&mappinglocation=tr-ips-ses-data|mappings|version_1_2|wos.mapping&datalocation=tr-ips-ses-data|json-data|wos|20150724|wos-1&port=9200&index=wos4&protocol=http&type=wos&access=access_key&secret=secret_key
+                Delete Index
+                http://127.0.0.1:8001/delete/wos4&host=ip or DNS&port=9200
+                
+                with loading you must specify the load command as shown above
+                use & to seperate values
+                use = to seperate key value pairs
+                use | to insert \
+                """
+                                
 @route('/get_status/<name>')
 def get_status( name="Get Loading Status"):
     values = name.split('&')
